0b83130d383479d53436e415a254452686a0add9
FusionLabs <juniornoumedem02@gmail.com>
2024-08-31 05:06:28 +0200

    Begin react partie 1 fin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,21 +12,39 @@ function ShoeCard(props) {
       <div className="card-body">
         <div className="flex items-center gap-2">
           <h2 className="card-title">{props.tile}</h2>
-          <div className="badge badge-primary"> new </div>
+          {/* <div className="badge badge-primary"> new </div> */}
           {/* <div className="card-actions justify-end"></div> */}
+          {props.isNew ? (
+            <div className="badge badge-primary"> new </div>
+          ) : null}
         </div>
       </div>
     </div>
   );
 }
+const PAPPER = [
+  { image: "/img/1.jpg", isNew: true, tile: "Dev FullStack" },
+  { image: "/img/2.jpg", isVisible: true, tile: "Dev FullStack junior" },
+  { image: "/img/3.jpg", tile: "Gojo Wallpeper" },
+  { image: "/img/4.jpg", isNew: true, tile: "Gojo" },
+];
+
+// export default function App() {
+//   return (
+//     <div className="flex flex-col items-center gap-5">
+//       {PAPPER.map((papper) => (
+//    return <ShoeCard {...papper}/>
+//       ))}
+//     </div>
+//   );
+// }
 
 export default function App() {
   return (
     <div className="flex flex-col items-center gap-5">
-      <ShoeCard image="/img/1.jpg" tile="Dev FullStack" />
-      <ShoeCard image="/img/2.jpg" tile="Dev FullStack junior" />
-      <ShoeCard image="/img/3.jpg" tile="Gojo Wallpeper" />
-      <ShoeCard image="/img/4.jpg" tile="Gojo " />
+      {PAPPER.map((papper, index) => (
+        <ShoeCard key={index} {...papper} />
+      ))}
     </div>
   );
 }
